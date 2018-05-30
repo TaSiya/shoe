c83d82561af00b0d00f4346926aad1a6da633f22
TaSiya <siyandamakhalima.1@gmail.com>
2018-05-30 10:30:02 +0200

    added second test for the search. I also created a function with key name inStock, inStock assigns the whatEver stored in local storage to the shoe map

--- test/shoe-catalogue.test.js
+++ test/shoe-catalogue.test.js
@@ -20,9 +20,13 @@ describe('Shoe Catalogue application Tests', function(){
             size  : 4,
             in_stock : 5
          }
-      ];
+
+      search.inStock(stock); //assign the list to the stock or map of the shoes
       it('brand : amateki color: black size :6', function(){
          assert.deepEqual(search.searchShoe('amateki', 'black', 6), [{brand : 'amateki',color : 'black',size : 6,in_stock : 15}]);
-      });
-   });
+      });//search 1
+      it('band :adidas color :red size :8', function(){
+         assert.deepEqual(search.searchShoe('adidas','red',8),[]);
+      });//search 2
+   });//search for a shoe
 });//End of application test

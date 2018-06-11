5cd09567866fc2695836c6be10538a6bf7444eb2
TaSiya <siyandamakhalima.1@gmail.com>
2018-06-11 14:52:09 +0200

    referenced all the dom elements

---
+++ js/shoe-dom.js
@@ -0,0 +1,19 @@
+// All References for the show Catalogue
+
+var stockTemplate = document.querySelector(.'stockTemplate').innerHTML; //template reference
+var card = document.querySelector('.card'); // cart
+var myStockDisplay = document.querySelector('.myStockDisplay');
+
+// input references
+var brandAdd = document.querySelector('.brandAdd');
+var colourAdd = document.querySelector('.colourAdd');
+var sizeAdd = document.querySelector('.sizeAdd');
+var stockAdd = document.querySelector('.stockAdd');
+
+var addToStockBtn = document.querySelector('.addToStockBtn');
+
+var brandSearch = document.querySelector('.brandToSearch');
+var colourSearch = document.querySelector('.colourSearch');
+var sizeSearch = document.querySelector('.sizeSearch');
+
+var searchShoeBtn = document.querySelector('.searchShoeBtn');
